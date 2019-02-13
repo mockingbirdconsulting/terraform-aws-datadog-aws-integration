b1ad955bc7b57e5d0e6f3f056fbf9bdabdf0fe65
Matthew Macdonald-Wallace <matt@mockingbirdconsulting.co.uk>
2019-02-13 20:46:21 +0000

    Update policies and move to correct files

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudtrail" "dev_cloudtrail" {
     name = "${var.log_bucket_name}"
     s3_bucket_name = "${aws_s3_bucket.cloudtrail_logs.id}"
+    include_global_service_events = true
 }
--- policies.tf
+++ policies.tf
@@ -128,7 +128,21 @@ resource "aws_s3_bucket_policy" "datadog_log_read_cloudtrail" {
             "arn:aws:s3:::${var.log_bucket_name}",
             "arn:aws:s3:::${var.log_bucket_name}/*"
         ]
-    }
+    },
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "cloudtrail.amazonaws.com"},
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::${var.log_bucket_name}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite20150319",
+            "Effect": "Allow",
+            "Principal": {"Service": "cloudtrail.amazonaws.com"},
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${var.log_bucket_name}/AWSLogs/${var.current_account_id}/*",
+            "Condition": {"StringEquals": {"s3:x-amz-acl": "bucket-owner-full-control"}}
+        }
   ]
 }
 EOF
